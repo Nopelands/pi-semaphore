3ac3b125a9ba0d4656eac96f742e14926645c7a2
phoenix <rhas@cin.ufpe.br>
2022-08-05 16:24:07 -0300

    final version

--- projeto.py
+++ projeto.py
@@ -1,17 +1,17 @@
 import RPi.GPIO as GPIO
 import time
 
-green = 1
-red = 2
-yellow = 3
+green = 15
+red = 11
+yellow = 13
 
-buzzer = 4
+buzzer = 40
 
-display1 = [5, 6, 7, 8, 9, 10, 11]
-display2 = [12, 13, 14, 15, 16, 17, 18]
+display1 = [23, 29, 31, 33, 35, 37, 12]
+display2 = [16, 18, 22, 24, 26, 32, 36]
 
 trigger = 19
-echo = 20
+echo = 21
 
 sete_segmentos = [
     [ 1,1,1,1,1,1,0 ], # = Digito 0
@@ -26,6 +26,7 @@ sete_segmentos = [
     [ 1,1,1,1,0,1,1 ], # = Digito 9
 ]
 
+GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(red, GPIO.OUT)
 GPIO.setup(green, GPIO.OUT)
@@ -38,6 +39,9 @@ for output in display1:
 for output in display2:
     GPIO.setup(output, GPIO.OUT)
 
+buzz = GPIO.PWM(buzzer, 440)
+buzz.start(0)
+
 def distance():
     GPIO.output(trigger, True)
     # set Trigger after 0.01ms to LOW
@@ -45,10 +49,11 @@ def distance():
     GPIO.output(trigger, False)
     StartTime = time.time()
     StopTime = time.time()
-
+    initial_time = time.time()
     while GPIO.input(echo) == 0:
         StartTime = time.time()
 
+    initial_time = time.time()
     while GPIO.input(echo) == 1:
         StopTime = time.time()
 
@@ -62,7 +67,7 @@ def display_write(centimeters):
     if centimeters > 99:
         centimeters = 99
     number1 = int(centimeters/10)
-    number2 = centimeters % 10
+    number2 = int(centimeters % 10)
     segmentos1 = sete_segmentos[number1]
     segmentos2 = sete_segmentos[number2]
     for i in range(len(display1)):
@@ -70,22 +75,29 @@ def display_write(centimeters):
     for i in range(len(display2)):
         GPIO.output(display2[i], segmentos2[i])
 
-while True:
+while True:    
     dist = distance()
     display_write(dist)
-    if dist == 0:
+    print(dist)
+    if dist < 0:
+        dist = 0
+    if dist > 99:
+        dist = 99
+    
+    if dist <= 5:
+        GPIO.output(buzzer, 1)
         GPIO.output(red, 1)
         GPIO.output(yellow, 1)
         GPIO.output(green, 1)
-        time.sleep(1)
+        time.sleep(0.25)
         GPIO.output(red, 0)
         GPIO.output(yellow, 0)
         GPIO.output(green, 0)
-    elif dist <= 33:
+    elif dist <= 15:
         GPIO.output(red, 1)
         GPIO.output(yellow, 0)
         GPIO.output(green, 0)
-    elif dist <= 66:
+    elif dist <= 30:
         GPIO.output(red, 0)
         GPIO.output(yellow, 1)
         GPIO.output(green, 0)
@@ -93,4 +105,10 @@ while True:
         GPIO.output(red, 0)
         GPIO.output(yellow, 0)
         GPIO.output(green, 1)
+
+    buzz.ChangeDutyCycle(99-dist)
+    time.sleep(dist/200)
+    if dist > 5:
+        buzz.ChangeDutyCycle(0)
+        time.sleep(dist/200)
     
