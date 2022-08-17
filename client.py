2157f7b0bed447f48a4d1b05d3f9ac16a3a83c5f
Samuel <samuelbsantosn@gmail.com>
2022-08-17 16:47:23 -0300

    Add files via upload

---
+++ client.py
@@ -0,0 +1,35 @@
+import json
+import socket
+
+print('Escreva um dos comandos:')
+print('1. SETUP \'nome do usuário(sem espaço)\' \'tempo led vermelho\' \'tempo led amarelo\' \'tempo led verde\'')
+print('2. GETDATA \'tipo de log a ser recolhido (cars ou setup)\'')
+texts = input().split(' ')
+command = texts[0]
+mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+mysock.connect(('127.0.0.1', 2035))
+if command == 'SETUP':
+    username = texts[1]
+    colors = (int(texts[2]), int(texts[3]), int(texts[4]))
+    struct = {
+        'command': command,
+        'username': username,
+        'red_time': colors[0],
+        'yellow_time': colors[1],
+        'green_time': colors[2]
+    }
+    data = bytes(json.dumps(struct), 'utf-8')
+    mysock.sendall(data)
+elif command == 'GETDATA':
+    log_type: texts[1]
+    struct = {
+        'command': command,
+        'log_type': log_type
+    }
+    data = bytes(json.dumps(struct), 'utf-8')
+    mysock.sendall(data)
+    data = mysock.recv(4096)
+    decoded_data = data.decode('utf-8')
+    struct = json.loads(decoded_data)
+    print('Dados recolhidos:')
+    print(struct['log'])
