5597c1f62a378be42684002065ac49dc7ebc7b93
phoenix <rhas@cin.ufpe.br>
2022-08-19 15:45:15 -0300

    added server and logging

--- projeto.py
+++ projeto.py
@@ -1,7 +1,9 @@
 import RPi.GPIO as GPIO
 import time
 import json
-import socket
+import socket as s
+import os
+import datetime
 
 green = 15
 red = 11
@@ -32,10 +34,25 @@ GPIO.setup(yellow, GPIO.OUT)
 GPIO.setup(mov_sensor, GPIO.IN)
 for output in display1:
     GPIO.setup(output, GPIO.OUT)
+    
+car_log = open("car_log.txt", "a+")
+    
+socket = s.socket(s.AF_INET, s.SOCK_STREAM)
+try:
+    socket.bind(('127.0.0.1', 2035))
+except s.error:
+    print("Failed to bind")
+    sys.exit()
+socket.setblocking(False)
+socket.listen()
 
-def get_config():
-    pass
-
+def get_config(json_data):
+    config = [json_data["red_time"], json_data["yellow_time"], json_data["green_time"]]
+    user_log = open("user_log.txt", "a")
+    user_log.write(f"{json_data['username']} - {datetime.datetime.now()}")
+    user_log.close()
+    return config
+    
 def reply_request():
     pass
 
@@ -47,40 +64,60 @@ def display_write(time):
     for i in range(len(display1)):
         GPIO.output(display1[i], segmentos1[i])
 
-red_time = 1
-yellow_time = 1
-green_time = 1
+config = [1, 1, 1]
 state = "red"
 state_time = 0
 previous_movement = 0
-car_log = {"red": 0, "green": 0}
-config_log = {"name": [], "time": []}
+car_log = open("car_log.txt", "r")
+car_log_count = [int(i) for i in car_log.read().split(" ")]
+car_log.close()
 
-while True:    
-    config = get_config() # if socket?
-    reply_request() # if?
+while True:
+    try:
+        conn,addr = socket.accept()
+        data = conn.recv(4096)
+        json_data = json.loads(data)
+        print(json_data)
+        if json_data["command"] == "SETUP":
+            config = get_config(json_data)
+        elif json_data["command"] == "GETDATA":
+            reply_request(json_data)
+    except:
+        pass
+    red_time = config[0]
+    yellow_time = config[1]
+    green_time = config[2]
+    print(state)
     if state == "red":
         time.sleep(0.1)
         state_time += 0.1
-        if state_time == red_time:
+        if state_time >= red_time:
             state = "green"
             state_time = 0
             previous_movement = 0
     elif state == "yellow":
-        time.sleep(yellow_time)
-        state = "red"
-        state_time = 0
+        time.sleep(0.1)
+        state_time += 0.1
+        if state_time >= yellow_time:
+            state = "red"
+            state_time = 0
+            previous_movement = 0
     elif state == "green":
         time.sleep(0.1)
         state_time += 0.1
         display_write(int(green_time - state_time))
-        if state_time == green_time:
+        if state_time >= green_time:
             state = "yellow"
             state_time = 0
             previous_movement = 0
-
     mov_read = GPIO.input(mov_sensor)
     if mov_read != previous_movement and previous_movement == 0 and state != "yellow":
-        car_log[state] += 1
+        if state == "red":
+            car_log_count[0] += 1
+        else:
+            car_log_count[1] += 1
+        car_log = open("car_log.txt", "w")
+        car_log.write(f"{car_log_count[0]} {car_log_count[1]}")
+        car_log.close()
+        print(car_log_count)
     previous_movement = mov_read
-
